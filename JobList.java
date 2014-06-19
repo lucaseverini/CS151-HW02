5884842daa674afca4f7b761a9e56ed1e572928d
Dennis <denniseh7@users.noreply.github.com>
2014-06-19 13:23:11 -0700

    Update JobList.java

--- JobList.java
+++ JobList.java
@@ -3,13 +3,16 @@ import java.util.Iterator;
 import java.util.Set;
 
 
+
 public class JobList 
 {
     private HashMap jobList;
+    private int count;
     
     public JobList()
     {
         jobList = new HashMap<Integer,Job>();
+        count=0;
     }
     
     public void addJob(int jobId, String jobName, String jobDepartment, String jobDescription)
@@ -26,13 +29,24 @@ public class JobList
     public void displayJobs()
     {
         Set keys=jobList.keySet();
-        
-        for (Iterator i=keys.iterator(); i.hasNext();)
-        {         
-            int key=(int)i.next();
-            Job nextJob=(Job) jobList.get(key);
-            System.out.printf("JobID:%d\nJob:%s\nDepartment:%s\nDescription:%s\n\n",nextJob.getId(),nextJob.getName(),nextJob.getDepartment(),nextJob.getDescription());
+        int size=jobList.size();
+        if (size > 0)
+        {
+            for (Iterator i=keys.iterator(); i.hasNext();)
+            {         
+                int key=(int)i.next();
+                Job nextJob=(Job) jobList.get(key);
+                System.out.printf("JobID:%d\nJob:%s\nDepartment:%s\nDescription:%s\n\n",nextJob.getId(),nextJob.getName(),nextJob.getDepartment(),nextJob.getDescription());
+            }
         }
-     
+        else
+        {
+            System.out.println("No Jobs on List");
+        }
+    }
+    
+    public int getJobSize()
+    {
+        return jobList.size();
     }
 }
