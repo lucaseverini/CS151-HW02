98a86f0650618ceb2941e177c51eb645672a03e5
Dennis <denniseh7@users.noreply.github.com>
2014-06-19 14:45:15 -0700

    Update Interviewer.java

--- Interviewer.java
+++ Interviewer.java
@@ -26,4 +26,9 @@ public class Interviewer {
     public void setName(String name) {
         this.name = name;
     }
+    @Override
+    public  String toString()
+    {
+        return "interviewer " + name;
+    }
 }
