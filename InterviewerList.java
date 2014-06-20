11f1994dfdafed55e7f8c34eed3841acb367c1d4
Dennis <denniseh7@users.noreply.github.com>
2014-06-19 18:19:44 -0700

    Update InterviewerList.java

--- InterviewerList.java
+++ InterviewerList.java
@@ -1,21 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package HireMe;
+package hireme;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Iterator;
+import java.util.*;
+
 /**
  *
  * @author Hylix
  */
 public class InterviewerList {
-    private HashMap myMap = new HashMap();
+    private HashMap<String,Interviewer> myMap;  
     private int id = 0;
     
-    public InterviewerList() {
+    public InterviewerList() 
+    {
+        myMap = new HashMap();
     }
     
     public InterviewerList(HashMap newMap) {
@@ -23,7 +20,9 @@ public class InterviewerList {
     }
     
     public void addInterviewer(String name){
-        myMap.put(name, name);
+        Interviewer i =  new Interviewer(name);
+        myMap.put(name, i);
+           
         id++;
     }
     
@@ -36,24 +35,27 @@ public class InterviewerList {
         return myMap.size();
     }
     
-    public void displayInterviewers()
+    public HashMap getInterviewerList()
     {
-        Set keys=myMap.keySet();
-        int size=myMap.size();
-        System.out.println("Interviewers:");
-        if (size > 0)
+        return myMap;
+    }
+    
+   public void displayInterviewers()
+    {
+        List<Interviewer> allInterviewers = new ArrayList(myMap.values());
+        
+        if (allInterviewers.size()>0)
         {
-            for (Iterator i=keys.iterator(); i.hasNext();)
-            {         
-                int key=(int)i.next();
-                Interviewer nextInterviewer=(Interviewer) myMap.get(key);
-                System.out.printf("%s\n", nextInterviewer.getName());                
+            System.out.println("Interviewers:");
+            for(Interviewer iter : allInterviewers)
+            {              
+                System.out.print(iter);
+                System.out.println("");
             }
-            System.out.println();
         }
         else
         {
-            System.out.println("No Jobs on List");
+            System.out.println("No Interviewers on List");
         }
     }
 }
